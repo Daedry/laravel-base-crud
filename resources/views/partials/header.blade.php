02321e946807c5391213ede4c8c07b4f6129df22
MyUsername <romamargine@gmail.com>
2022-06-16 16:43:08 +0200

    Add fix header links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header>
     <nav class="navbar navbar-expand navbar-light bg-light justify-content-center">
         <div class="nav navbar-nav ">
-            <a class="nav-item nav-link active" href="#">Home <span class="visually-hidden">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Comics</a>
-            <a class="nav-item nav-link" href="#">Contacts</a>
+            <a class="nav-item nav-link active" href="{{ route('home') }}">Home <span class="visually-hidden">(current)</span></a>
+            <a class="nav-item nav-link" href="{{ route('comics') }}">Comics</a>
+            <a class="nav-item nav-link" href="{{ route('home') }}">Contacts</a>
         </div>
     </nav>
 </header>
