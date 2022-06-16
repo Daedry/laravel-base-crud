681036147cec9a1960f1f4233857d6c5ef18acbb
MyUsername <romamargine@gmail.com>
2022-06-16 15:50:52 +0200

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+<nav class="navbar navbar-expand navbar-light bg-light justify-content-center">
+    <div class="nav navbar-nav ">
+        <a class="nav-item nav-link active" href="#">Home <span class="visually-hidden">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Comics</a>
+        <a class="nav-item nav-link" href="#">Contacts</a>
+    </div>
+</nav>
+    </nav>
+
+</header>
