4533f2a1609b9917ae7c5fb1295775319c5aaa97
MyUsername <romamargine@gmail.com>
2022-06-16 15:51:24 +0200

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <header></header>
+        @include('partials.header')
 
         <main>
             @yield('content')
