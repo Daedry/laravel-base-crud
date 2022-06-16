5c9115ba7348bdc747a58005de53796a51a2539d
MyUsername <romamargine@gmail.com>
2022-06-16 17:04:46 +0200

    Add dinamically comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 
-<h1>COMICS PAGE</h1>
+<div class="container-fluid">
+    <div class="wrapper bg-primary">
+        <div class="container">
+            <h1 class="p-4">Comics</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="cards my-5">
+            <div class="row row-cols-2 row-cols-sm-3 row-cols-md-5 g-4">
+                @forelse($comics as $comic)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-top">
+                            <img class="card-img-top img-fluid" src="{{ $comic->thumb }}" alt="Card image cap">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <!-- <p class="card-text">{{ $comic->description }}</p> -->
+                            <a href="" class="btn btn-primary">Show</a>
+                        </div>
+                    </div>
+                </div>
+                @empty
+                <p>No comics found</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="nav navbar-nav ">
             <a class="nav-item nav-link active" href="{{ route('home') }}">Home <span class="visually-hidden">(current)</span></a>
             <a class="nav-item nav-link" href="{{ route('comics') }}">Comics</a>
-            <a class="nav-item nav-link" href="{{ route('home') }}">Contacts</a>
+            <a class="nav-item nav-link" href="{{ route('home') }}">Create Comics</a>
         </div>
     </nav>
 </header>
