282edf3576be925e0e93b4ef45268e59bcf9d672
MyUsername <romamargine@gmail.com>
2022-06-16 16:03:56 +0200

    Add Fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,9 @@
 <header>
-<nav class="navbar navbar-expand navbar-light bg-light justify-content-center">
-    <div class="nav navbar-nav ">
-        <a class="nav-item nav-link active" href="#">Home <span class="visually-hidden">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Comics</a>
-        <a class="nav-item nav-link" href="#">Contacts</a>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand navbar-light bg-light justify-content-center">
+        <div class="nav navbar-nav ">
+            <a class="nav-item nav-link active" href="#">Home <span class="visually-hidden">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Comics</a>
+            <a class="nav-item nav-link" href="#">Contacts</a>
+        </div>
     </nav>
-
 </header>
